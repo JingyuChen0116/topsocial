4a8906970866bb3e777ca657c9f5cad11873409f
JingyuChen0116 <102499110+JingyuChen0116@users.noreply.github.com>
2023-08-03 18:39:14 +1000

    add dom

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import "./index.css"
+import { profile } from "./data/db.json"
 // import { cards, message, request, profile, highline } from "@/js/database"
+import {
+  navbar,
+  navbar_container,
+  navbar_container_logo,
+  navbar_container_searchbar,
+  navbar_container_create,
+  navbar_container_create_img
+} from '@/js/common'
 
 /* NAV */
+navbar_container_logo.textContent = "TopSocial"
+navbar_container_create_img.src = profile.img_src
 
 /* LEFT */
 
