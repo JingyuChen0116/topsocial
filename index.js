0d4a7cde86532aeba0e2af2684d0fb42ddc68703
JingyuChen0116 <102499110+JingyuChen0116@users.noreply.github.com>
2023-08-04 13:06:25 +1000

    add left structure

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import "./index.css"
 import { profile } from "./data/db.json"
 // import { cards, message, request, profile, highline } from "@/js/database"
 import {
-  navbar,
-  navbar_container,
+  // NavBar
   navbar_container_logo,
-  navbar_container_searchbar,
-  navbar_container_create,
-  navbar_container_create_img
+  navbar_container_create_img,
+  // Main Left
+  left_profile_img,
+  left_profile_name,
+  left_profile_at
 } from '@/js/common'
 
 /* NAV */
@@ -15,6 +16,9 @@ navbar_container_logo.textContent = "TopSocial"
 navbar_container_create_img.src = profile.img_src
 
 /* LEFT */
+left_profile_img.src = navbar_container_create_img.src
+left_profile_name.textContent = profile.name
+left_profile_at.textContent = profile.at
 
 /* MIDDLE */
 
--- src/js/common.js
+++ src/js/common.js
@@ -7,9 +7,16 @@ export const navbar_container_create = navbar_container.querySelector(".create")
 export const navbar_container_create_img = navbar_container.querySelector("img")
 
 /* Main */
+export const main = document.querySelector("main")
+export const main_container = main.querySelector(".container")
 
 
 /* Main Left */
+export const left = main_container.querySelector(".main-left")
+export const left_profile = left.querySelector(".profile")
+export const left_profile_img = left_profile.querySelector("img")
+export const left_profile_name = left_profile.querySelector("h2")
+export const left_profile_at = left_profile.querySelector("p")
 
 
 /* Main Middle */
